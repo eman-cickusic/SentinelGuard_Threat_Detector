c78c83aae85edeeb257b68b9cb8d453b214636d9
Eman Cickusic <155198695+emanhz@users.noreply.github.com>
2025-01-14 18:31:08 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from basic_threat_detector import EnhancedThreatDetector
-
-if __name__ == "__main__":
-    with open('config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
-
-    detector = EnhancedThreatDetector(config)
-    detector.start_monitoring()
