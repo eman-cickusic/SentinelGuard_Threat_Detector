aca08b2ec9252f49eca5e2184354f14d5754fbde
Eman Cickusic <155198695+emanhz@users.noreply.github.com>
2025-01-15 22:16:16 +0100

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# This file can be empty - it's used to mark the directory as a Python package
+# It allows you to use imports from this directory
---
+++ src/utils.py
@@ -0,0 +1,24 @@
+from scapy.all import TCP, UDP, DNS
+
+def get_protocol(packet):
+    if TCP in packet:
+        return 'TCP'
+    elif UDP in packet:
+        return 'UDP'
+    elif DNS in packet:
+        return 'DNS'
+    return 'OTHER'
+
+def get_source_port(packet):
+    if TCP in packet:
+        return packet[TCP].sport
+    elif UDP in packet:
+        return packet[UDP].sport
+    return None
+
+def get_destination_port(packet):
+    if TCP in packet:
+        return packet[TCP].dport
+    elif UDP in packet:
+        return packet[UDP].dport
+    return None
